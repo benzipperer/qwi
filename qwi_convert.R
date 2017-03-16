dbe1089b3506b95b11940536182bc89051d77204
Ben Zipperer <benzipperer@gmail.com>
2017-03-16 12:41:36 -0400

    adding geography, demo group lists

--- qwi_convert.R
+++ qwi_convert.R
@@ -14,23 +14,35 @@ version <- "R2016Q4"
 
 # directory structure
 home <- file.path("/data/qwi")
+code <- file.path(home,"code")
 rawdata <- file.path(home,"raw",version)
-statadata <- file.path(home,"stata",version)
-filesep <- .Platform$file.sep
+statadata <- file.path(home,"exports",version)
 
 # Set number of cores to use
-registerDoMC(cores=10)
+registerDoMC(cores=8)
+
+# multicore options
+mcoptions <- list(preschedule=FALSE)
+
 
 # states to process
-allstates <- read_csv(paste0(code,filesep,"allstates.csv"))
-allstates <- allstates %>% transmute(stateabb=tolower(stateabb))
-# filter available states in this release from all states
-availablestates <- filter(allstates,stateabb!="id",stateabb!="mi",stateabb!="wy")
+#allstates <- read_csv(paste0(code,filesep,"allstates.csv"))
+allstates <- read_csv(file.path(code,"allstates.csv")) %>% transmute(stateabb=tolower(stateabb))
+# filter available states in this release from all states: R2016Q4
+availablestates <- filter(allstates,stateabb!="wy")
 # test the function below on small subset of states
-# availablestates <- filter(allstates,stateabb=="ak" | stateabb=="az")
+availablestates <- filter(allstates,stateabb=="ak" | stateabb=="az")
 
 # ownership categories to process
 ownlist <- c("oslp","op")
+
+# geography categories to process
+#geolist <- c("gs","gc")
+geolist <- c("gs")
+
+# demographic groups to process
+grouplist <- c("rh","sa")
+
 
 # define function to download, convert, compress
 # should probably be using temp files in it but haven't done that yet
@@ -38,15 +50,21 @@ ownlist <- c("oslp","op")
 myfunction <- function(x) {
   
   # preliminaries
+  
+  # categories to loop over
   stateabb <- x$stateabb
   own <- x$own
-  basefile <- paste0("qwi_",stateabb,"_sa_f_gc_n3_",own,"_u")
+  geo <- x$geo
+  group <- x$group
+  
+  # filenames and directories
+  basefile <- paste0("qwi_",stateabb,"_",group,"_f_",geo,"_n3_",own,"_u")
   rawfile <- paste0(basefile,".csv.gz")
   statafile <- paste0(basefile,".dta")
   finalfile <- paste0(basefile,".dta.zip")
-  url <- paste0("https://lehd.ces.census.gov/pub/",stateabb,"/",version,"/DVD-sa_f/",rawfile)
-  rawdest <- paste0(rawdata,filesep,rawfile)
-  finaldest <- paste0(statadata,filesep,finalfile)
+  url <- paste0("https://lehd.ces.census.gov/pub/",stateabb,"/",version,"/DVD-",group,"_f/",rawfile)
+  rawdest <- file.path(rawdata,rawfile)
+  finaldest <- file.path(statadata,finalfile)
  
    # download data
   system(paste("echo","Downloading ",url))
@@ -64,8 +82,10 @@ myfunction <- function(x) {
   file.remove(statafile,finalfile)
 }
 
-mygrid <- expand.grid(stateabb=availablestates$stateabb,own=ownlist)
-mcoptions <- list(preschedule=FALSE)
+# create the grid for foreach
+mygrid <- expand.grid(stateabb=availablestates$stateabb,own=ownlist,geo=geolist,group=grouplist)
+
+# process the data
 foreach(i=iter(mygrid, by="row"), .options.multicore=mcoptions) %dopar% myfunction(i)
 
 
